a88af8170136d863d096ea2144a87069bf57b871
KamanHang <kamanhangshrong@gmail.com>
2025-10-11 11:56:48 +0545

    INITIALIZE PORTFOILIO

---
+++ src/app/app.routes.ts
@@ -0,0 +1,39 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+
+  {
+    path: '',
+    loadComponent: () =>
+      import('../app/components/shell/shell-component/shell-component').then(
+        (m) => m.ShellComponent
+      ),
+      children: [
+         {
+    path: 'home',
+    loadComponent: () =>
+      import('../app/components/home/home').then(
+        (m) => m.Home
+      ),
+  },
+
+  {
+    path: 'projects',
+    loadComponent: () =>
+      import('../app/components/projects/projects').then(
+        (m) => m.Projects
+      ),
+  },
+
+
+  {
+    path: 'aboutme',
+    loadComponent: () =>
+      import('../app/components/about-me/about-me').then(
+        (m) => m.AboutMe
+      ),
+  },
+      ]
+  },
+
+];
