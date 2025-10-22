43e94d446db615d67b5c09756ab7650c8d72a674
kaman <kaman@vertexspecial.com>
2025-10-22 18:18:23 +0545

    ADDED: SEE MORE LESS

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,14 @@ import Lara from '@primeuix/themes/lara';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { providePrimeNG } from 'primeng/config';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
+    provideAnimationsAsync(),
     provideRouter(routes), provideClientHydration(withEventReplay()),
      providePrimeNG({
             theme: {
